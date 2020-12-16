2a35b88a658a9e90839899401b464f71ccebf09c
Weasel On A Stick <korotkih.00@gmail.com>
2020-12-16 20:11:05 +0700

    1.0.1

--- __init__.py
+++ __init__.py
@@ -14,7 +14,7 @@ from . import utils_bpy
 import inspect
 bl_info = {'name': "WMDE - Weasel's Map Data Editor",
            'author': 'Weasel On A Stick',
-           'version': (1, 0, 0),
+           'version': (1, 0, 1),
            'blender': (2, 82, 7),
            'location': 'View3D > Sidebar > WMDE',
            'description': 'Edit SHAR map data, including: roads, paths, fences and locators',
